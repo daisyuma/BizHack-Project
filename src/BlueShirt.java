cba00dff259221ed7996e333edd3df88cce6bfef
Daisy <daisy88129@gmail.com>
2020-01-31 21:53:53 -0800

    first commit

--- src/BlueShirt.java
+++ src/BlueShirt.java
@@ -1,8 +1,25 @@
 public class BlueShirt {
 
-    int j;
+    private Customer customer;
 
     public BlueShirt() {
+    }
+
+    public void setCustomer(Customer customer){
+        this.customer = customer;
+    }
+
+    public void askAssistance(){
 
     }
+
+    public void askProduct(){
+
+    }
+
+    public void askPurpose(){
+
+    }
+
+    public void
 }
